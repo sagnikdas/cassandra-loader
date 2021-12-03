040abcb0d7acc06fdd6340acbf08a3289d23c31f
sagnikdas <sagnikd91@gmail.com>
2021-12-04 01:56:33 +0530

    Initial commit

---
+++ src/main/java/com/cassandra/dataloader/cassandraloader/CassandraLoaderApplication.java
@@ -0,0 +1,25 @@
+package com.cassandra.dataloader.cassandraloader;
+
+import com.cassandra.dataloader.cassandraloader.connection.DataStaxAstraProperties;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import java.nio.file.Path;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+public class CassandraLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraLoaderApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+}
---
+++ src/test/java/com/cassandra/dataloader/cassandraloader/CassandraLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassandra.dataloader.cassandraloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
